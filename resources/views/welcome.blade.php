efbe971dca3f00b7fa154a754a2accdc1dba38b6
Sebastian Trujillo <juanstt99@gmail.com>
2020-02-14 10:55:46 -0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel gDD
+                    GRM UIS
                 </div>
 
                 <div class="links">
